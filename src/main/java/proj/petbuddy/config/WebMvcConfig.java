b24e05949d37beeadfd5076d5b950eaef67ca3b0
sol1224 <129352956+sol1224@users.noreply.github.com>
2024-05-04 16:46:55 +0900

    url update

--- src/main/java/proj/petbuddy/config/WebMvcConfig.java
+++ src/main/java/proj/petbuddy/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package proj.petbuddy.config;
+//package proj.petbuddy.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
