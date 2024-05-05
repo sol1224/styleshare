478045ebe7d341541a7a632fe84bfbba5393cf61
sol1224 <129352956+sol1224@users.noreply.github.com>
2024-05-06 02:13:33 +0900

    url update

--- src/main/java/proj/petbuddy/config/WebMvcConfig.java
+++ src/main/java/proj/petbuddy/config/WebMvcConfig.java
@@ -9,14 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
 
-    //application.properties에 설정한 "uploadPath" 프로퍼티 값을 읽어옴
-    @Value("${uploadPath}")
-    String uploadPath;
+//    //application.properties에 설정한 "uploadPath" 프로퍼티 값을 읽어옴
+//    @Value("${uploadPath}")
+//    String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/item/**") // 브라우저 url에 /images로 시작하는 경우 uploadPath에 설정한 폴더를 기준으로 파일을 읽어옴
-                .addResourceLocations(uploadPath); //로컬 컴퓨터에 저장된 파일을 읽어올 root 경로를 설정
+//                .addResourceLocations(uploadPath); //로컬 컴퓨터에 저장된 파일을 읽어올 root 경로를 설정
+                .addResourceLocations("file:/img/"); //로컬 컴퓨터에 저장된 파일을 읽어올 root 경로를 설정
     }
 
 }
